0ed8d594559fb0ad30284b2ea3ce5f358dc38e90
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2022-10-26 06:30:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6285852939370','6285852939370','6285852939370']
-global.premium = ['6285852939370']
+global.owner = ['6282223011829','6282223011829','6282223011829']
+global.premium = ['6282223011829']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
